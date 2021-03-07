1d7b17aab2df33771dd4bc4c307d1332be9649b4
motoki-shun <motoki-shun@kayac.com>
2021-03-07 17:41:48 +0900

    bump lock version

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,5 +1,5 @@
 # config valid for current version and patch releases of Capistrano
-lock "~> 3.15.0"
+lock "~> 3.16.0"
 
 set :application, "capistrano-study"
 set :repo_url, "git@github.com:shuuuuun/sinatra-starter.git"
